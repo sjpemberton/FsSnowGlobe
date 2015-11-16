37fab71564d215384f42c498d6c55b6b601a0962
Steve Pemberton <sjpemberton09@gmail.com>
2015-11-16 22:12:07 +0000

    Initial Commit

---
+++ FsSnowGlobe/GlobeViewModel.fs
@@ -0,0 +1,12 @@
+﻿namespace ViewModels
+
+open System
+open System.Windows
+open FSharp.ViewModule
+open FSharp.ViewModule.Validation
+open FsXaml
+
+type GlobeView = XAML<"MainWindow.xaml", true>
+
+type GlobeViewModel() as this = 
+    inherit ViewModelBase()
---
+++ FsSnowGlobe/MainWindow.xaml.fs
@@ -0,0 +1,18 @@
+﻿namespace ViewModels
+
+open System
+open System.Windows
+open FSharp.ViewModule
+open FSharp.ViewModule.Validation
+open FsXaml
+
+type MainView = XAML<"MainWindow.xaml", true>
+
+type MainViewModel() as this = 
+    inherit ViewModelBase()
+
+    let exitCommand =  this.Factory.CommandSync (fun () ->  Application.Current.Shutdown()) 
+    let dragComand = this.Factory.CommandSync (fun () -> Application.Current.MainWindow.DragMove())
+
+    member x.ExitCommand = exitCommand
+    member x.DragCommand = dragComand
