6774c1c989b57776188ecdb52689c4bc0625352d
Steve Pemberton <sjpemberton09@gmail.com>
2015-11-20 00:19:11 +0000

    Added Particle proj and solution file

---
+++ ParticleEngine/Particle.fs
@@ -0,0 +1,30 @@
+﻿namespace ParticleEngine
+
+module particle =
+
+    type Point = {X:int; Y:int}
+    type Bounds = {Min:Point; Max:Point}
+
+    //type IEmitter = 
+    //    abstract Generate : unit -> Point
+    //
+    //type BoxEmitter(width, height) = 
+    //    interface IEmitter with
+    //        member x.Generate() = 
+    //            let rand = System.Random()
+    //            { X = rand.Next(0, width)
+    //              Y = rand.Next(0, height) }
+            
+
+    let Emit generator =
+        generator
+
+    let collides bounds point =
+        bounds.Min.X < point.X && bounds.Max.Y > point.Y
+        && bounds.Min.Y < point.Y && bounds.Max.Y > point.Y
+        
+    let BoxEmitter w h = 
+        let rand = System.Random()
+        Emit (fun () -> { X = rand.Next(0, w)
+                          Y = rand.Next(0, h) })
+
