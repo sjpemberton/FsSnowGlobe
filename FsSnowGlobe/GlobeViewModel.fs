c86034c5f78549c7dc37efb963e9d96a4fb391b0
Steve Pemberton <sjpemberton09@gmail.com>
2015-12-16 23:05:54 +0000

    Added F# background img

--- FsSnowGlobe/GlobeViewModel.fs
+++ FsSnowGlobe/GlobeViewModel.fs
@@ -10,30 +10,27 @@ open System.Windows.Input
 open Microsoft.Win32
 open System.Windows.Media.Imaging
 
-type NotifyingPoint(x,y,scale,rotation,alpha) as this = 
+type ParticleViewModel(x,y,scale,rotation,alpha) as this = 
     inherit ViewModelBase()
 
     let rotation = this.Factory.Backing(<@this.Rotation@>, rotation)
     let scale = this.Factory.Backing(<@this.Scale@>, scale)
-
     let alpha = this.Factory.Backing(<@this.Alpha@>, alpha)
-    let visible = this.Factory.Backing(<@this.Visible@>, true)
     let x = this.Factory.Backing(<@this.X@>, x)
     let y = this.Factory.Backing(<@this.Y@>, y)
 
     member this.Rotation with get () = rotation.Value and set (v) = rotation.Value <- v
     member this.Scale with get () = scale.Value and set (v) = scale.Value <- v
     member this.Alpha with get () = alpha.Value and set (v) = alpha.Value <- v
-    member this.Visible with get () = visible.Value and set (v) = visible.Value <- v
     member this.X with get () = x.Value and set (v) = x.Value <- v
     member this.Y with get () = y.Value and set (v) = y.Value <- v
 
 type GlobeViewModel() as this = 
     inherit EventViewModelBase<EngineEvent>() //If not using events, change base
 
-    let img = this.Factory.Backing(<@this.BackgroundImageSource@>, new BitmapImage())
-    let particles = ObservableCollection<NotifyingPoint>() //Snow
-    let mistParticles = ObservableCollection<NotifyingPoint>() //Mist
+    let img = this.Factory.Backing(<@this.BackgroundImageSource@>, new BitmapImage(new Uri(new Uri(System.AppDomain.CurrentDomain.BaseDirectory), "..\\..\\fsharp2048.png")))
+    let particles = ObservableCollection<ParticleViewModel>() //Snow
+    let mistParticles = ObservableCollection<ParticleViewModel>() //Mist
     let frameTimer = new DispatcherTimer()
 
     let createBackgroundImg () =
@@ -50,21 +47,20 @@ type GlobeViewModel() as this =
         Snow.Animation.RaiseMouseEvent mEvent), fun _ -> true) :> ICommand
         
     // Mutable state for UI
-    let updateParticleUI (collection: ObservableCollection<NotifyingPoint>) particles = 
+    let updateParticleUI (collection: ObservableCollection<ParticleViewModel>) particles = 
         particles 
         |> List.rev
         |> List.iteri (fun i p-> 
             match i,p with
             | x,_ when i < collection.Count ->
-                collection.[i].Visible <- true
                 collection.[i].X <- p.Coords.X
                 collection.[i].Y <- p.Coords.Y
                 collection.[i].Rotation <- p.Rotation
                 collection.[i].Alpha <- p.Alpha
             | _,_ ->
-                collection.Add(new NotifyingPoint(p.Coords.X, p.Coords.Y,p.Scale,p.Rotation, p.Alpha)))
+                collection.Add(new ParticleViewModel(p.Coords.X, p.Coords.Y,p.Scale,p.Rotation, p.Alpha)))
 
-    let onFrame (_, elapsed) =
+    let onTick (_, elapsed) =
         let snowState = Snow.Animation.Update elapsed
         let mistState = Mist.Animation.Update elapsed 
         snowState.Particles |> updateParticleUI particles
@@ -74,7 +70,7 @@ type GlobeViewModel() as this =
         frameTimer.Tick
         |> Observable.scan (fun (previous, elapsed) _  -> 
             (float Environment.TickCount, (float Environment.TickCount - previous) / 1000.0)) (float Environment.TickCount, float Environment.TickCount)
-        |> Observable.subscribe onFrame
+        |> Observable.subscribe onTick
         |> ignore
         frameTimer.Interval <- TimeSpan.FromSeconds(1.0 / 60.0);
         frameTimer.Start();
@@ -82,6 +78,6 @@ type GlobeViewModel() as this =
     member x.MouseCommand = mouseCommand
     member x.LoadImage = loadImgCommand
     member x.RaiseWindowMove(v:Vector) = Snow.Animation.RaiseMoveEvent v
-    member x.Particles: ObservableCollection<NotifyingPoint> = particles
-    member x.MistParticles: ObservableCollection<NotifyingPoint> = mistParticles
+    member x.Particles: ObservableCollection<ParticleViewModel> = particles
+    member x.MistParticles: ObservableCollection<ParticleViewModel> = mistParticles
     member x.BackgroundImageSource with get () = img.Value 
